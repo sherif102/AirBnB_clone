7812544cc46da7ed0b70f05ce99afdafc197f986
hyperbayse <hyperbayse@gmail.com>
2024-11-20 19:10:21 +0100

    .

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
 
     def __init__(self):
         """ initializes the BaseMOdel """
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
